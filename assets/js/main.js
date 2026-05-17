80373b1f32b6d83b0738a54c268d1ad05cfa548d
Triade Wisusanti <triadewisusanti12@gmail.com>
2026-05-17 22:46:03 +0700

    Perbaikan code: [jelaskan perbaikan apa]

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,13 +72,15 @@
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+  if (scrollTop) {
+    scrollTop.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
     });
-  });
+  }
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
